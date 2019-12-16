4b4dd0e3ccadf691a25be16ace60ad470ef777f9
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-16 13:49:21 +0530

    added sign in with google button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -35,23 +40,30 @@ class SignIn extends React.Component{
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input
+                    <FormInput
                         name='email' 
                         type='email' 
                         value={this.state.email} 
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='Email'
                         required 
                     />
-                    <label>Email</label>
-                    <input 
+                    <FormInput 
                         name='password'
                         type='password'
                         value={this.state.password}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='Password'
                         required 
                     />
-                    <label>Password</label>
-                    <input type='submit' value='Submit Form'/>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>
+                            Sign In
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         );
