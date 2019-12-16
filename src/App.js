445791dca368553a73c0a0e18b5e80bce2dcd033
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-16 13:47:35 +0530

    moved to class component and added firebase auth

--- src/App.js
+++ src/App.js
@@ -8,17 +8,45 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+import {auth} from './firebase/firebase.utils';
+
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state =  {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount (){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      });
+      console.log(user)
+    });
+  };
+
+  componentWillUnmount (){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
