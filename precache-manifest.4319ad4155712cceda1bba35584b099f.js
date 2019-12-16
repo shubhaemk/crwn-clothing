9393391a15819839ce61e9723b4435b5087166e8
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-16 15:08:11 +0530

    Updates

---
+++ precache-manifest.4319ad4155712cceda1bba35584b099f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cff60f148e7a3e9351bd45c2c83f22",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "f9ced828ccfa65d0548f",
+    "url": "/crwn-clothing/static/css/main.91eef8f7.chunk.css"
+  },
+  {
+    "revision": "b02c6aa4677110132a3a",
+    "url": "/crwn-clothing/static/js/2.a37f8b3b.chunk.js"
+  },
+  {
+    "revision": "f9ced828ccfa65d0548f",
+    "url": "/crwn-clothing/static/js/main.17ab6a50.chunk.js"
+  },
+  {
+    "revision": "10f85b2b9463efe22e26",
+    "url": "/crwn-clothing/static/js/runtime-main.910832d0.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
