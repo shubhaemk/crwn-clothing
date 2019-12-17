441675580935b955f44558634ca9d185ffc1503c
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-17 19:16:51 +0530

    added : signin redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -45,15 +45,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
-          <Route path='/*' component={SignInAndSignUp} />
+          <Route 
+            path='/signin' 
+            exact 
+            render= { () => 
+              this.props.currentUser ? 
+              (<Redirect to='/' />) 
+              :
+              (<SignInAndSignUp/>) } />
         </Switch>
       </div>
     )
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
