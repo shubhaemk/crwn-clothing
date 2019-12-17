6e2be8dffc13be2a016f74a756f5a3a2c753969f
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-17 09:51:17 +0530

    added : sign in with email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -17,13 +17,20 @@ class SignIn extends React.Component{
         };
     };
 
-    handleSubmit = event =>{
+    handleSubmit =  async event =>{
         event.preventDefault();
 
-        this.setState({
-            email: '',
-            password: ''
-        });
+        const { email, password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+                email: '',
+                password: ''
+            });
+        }catch(error){
+            console.log(error);
+        }
     };
 
     handleChange = event =>{
