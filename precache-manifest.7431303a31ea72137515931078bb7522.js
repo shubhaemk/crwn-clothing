a3373aa80ab95b3457996a319c9e9538bc704669
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-17 09:34:29 +0530

    Updates

--- precache-manifest.7431303a31ea72137515931078bb7522.js
+++ precache-manifest.7431303a31ea72137515931078bb7522.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98f5a19e5e7d0b71475395c1edd0dd48",
+    "revision": "4dfbb3febdff37f6b1ef9a2f8e3b3ea5",
     "url": "/crwn-clothing/index.html"
   },
   {
-    "revision": "77a488ab9074d111df18",
-    "url": "/crwn-clothing/static/css/main.91eef8f7.chunk.css"
+    "revision": "5ec0a44cf3ef3e6a453e",
+    "url": "/crwn-clothing/static/css/main.ffd3dd22.chunk.css"
   },
   {
-    "revision": "b02c6aa4677110132a3a",
-    "url": "/crwn-clothing/static/js/2.a37f8b3b.chunk.js"
+    "revision": "215fff81cd83d61dda86",
+    "url": "/crwn-clothing/static/js/2.f4cc05e1.chunk.js"
   },
   {
-    "revision": "77a488ab9074d111df18",
-    "url": "/crwn-clothing/static/js/main.103e98aa.chunk.js"
+    "revision": "5ec0a44cf3ef3e6a453e",
+    "url": "/crwn-clothing/static/js/main.c6d96f0c.chunk.js"
   },
   {
     "revision": "10f85b2b9463efe22e26",
