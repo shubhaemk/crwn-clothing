3f64d3885e02e96494a1d4061397d22801871c19
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-18 13:58:23 +0530

    added : selectors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { createStructuredSelector } from 'reselect';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -11,6 +11,7 @@ import Header from './components/header/header.component';
 
 import {auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector';
 
 
 class App extends React.Component {
@@ -59,8 +60,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser : user.currentUser
+const mapStateToProps = createStructuredSelector ({
+  currentUser : selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
@@ -16,9 +17,9 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
     </div>
 );
 
-const mapStateToProps = state => ({
+const mapStateToProps = createStructuredSelector ({
     //this is called selector
-    itemCount: selectCartItemsCount(state)
+    itemCount: selectCartItemsCount
 });
 
 const mapDispatchToProps = dispatch => ({
