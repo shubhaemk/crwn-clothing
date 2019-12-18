eda967583419ae8f223c064b6ac4647f7524cf64
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-18 16:49:28 +0530

    added : checkout page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { createStructuredSelector } from 'reselect';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import CheckOutPage from './pages/checkout/checkout.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import Header from './components/header/header.component';
 
 import {auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -46,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckOutPage} />
           <Route 
             path='/signin' 
             exact 
