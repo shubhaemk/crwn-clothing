656b344db1ff814366eac7d389a9d2f690b5bbef
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-18 13:05:54 +0530

    added : cart selector

---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+//1.st type of selector
+//selects a slice of store and returs it [ 1 level deep ]
+const selectCart = state => state.cart;
+
+//2nd type of selector
+//can go many level deep
+
+export const selectCartItems = createSelector(
+    [selectCart], //choose from which slice you want to have data    
+    cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumalatedQuantity, cartItem) =>
+            accumalatedQuantity + cartItem.quantity,
+        0
+    )
+)
