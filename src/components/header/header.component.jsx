431705a6f8b64dda4bf4be8e94663c056d765063
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-18 09:33:44 +0530

    added : toggle cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ( { currentUser, hidden } ) => (
     <div className="header">
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -34,12 +34,15 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div>
-        <CartDropdown></CartDropdown>
+        {
+            hidden ? null : (<CartDropdown></CartDropdown>)
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser : state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
