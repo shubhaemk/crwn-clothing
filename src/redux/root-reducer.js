d254e6b8dd74ad2a74568e6297206b5f7741eed3
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-19 09:54:23 +0530

    added : directory reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,16 +4,18 @@ import storage from 'redux-persist/lib/storage/';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['cart'/* ,'user' */]
+    whitelist: ['cart' /*,'directory','user' */]
 }
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
