ace9cdd28222dd8ef4fa706433fc06de7606e725
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-19 15:55:38 +0530

    refactored : moved some imports in order

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-
-import MenuItem from '../menu-item/menu-item.component';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
+import MenuItem from '../menu-item/menu-item.component';
+
 import './directory.styles.scss';
 
 const Directory = ({ sections }) => (
-    <div className="directory-menu">
-        {
-            sections.map(({ id, ...otherSectionProps }) => (
-                <MenuItem key={id} {...otherSectionProps}></MenuItem>
-            ))
-        }
+    <div className='directory-menu'>
+        {sections.map(({ id, ...otherSectionProps }) => (
+            <MenuItem key={id} {...otherSectionProps} />
+        ))}
     </div>
 );
 
